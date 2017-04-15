34f951285637e806bc734e7070f807c0ddbbeaab
ClementMoreauExia <clement.moreau1@viacesi.fr>
2017-04-15 11:46:30 +0200

    Maj design

--- resources/views/Design-Web/Header-Exia.php
+++ resources/views/Design-Web/Header-Exia.php
@@ -3,8 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <title>Header</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
+<div id="header">
+    <div id="top-header"></div>
+    <div id="header-body">
 
+        <div id="logoCesi">
+            <img src="image/logoExia.jpg">
+        </div>
+
+        <div id="logoBDE">
+            <img src="image/logoBDE.png">
+        </div>
+
+        <div id="ajout">
+            <img src="image/plus.png">
+        </div>
+
+        <div id="acceuil">
+            <div class="logo">
+                <img src="image/home.png">
+            </div>
+            <div class="titre">
+                <p>ACCUEIL</p>
+            </div>
+
+        </div>
+        <div id="activités">
+            <div class="logo">
+                <img src="image/activity.png">
+            </div>
+            <div class="titre">
+                <p><span>ACTIVITÉS</span></p>
+            </div>
+
+        </div>
+        <div id="boutique">
+            <div class="logo">
+                <img src="image/cadi.png">
+            </div>
+            <div class="titre">
+                <p>BOUTIQUE</p>
+            </div>
+
+        </div>
+    </div>
+</div>
 </body>
 </html>
